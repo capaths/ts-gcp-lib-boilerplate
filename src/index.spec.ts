9535879db8fd04b0003bfa5de248bafa8523e1fb
Paths <pcampaa@gmail.com>
2021-10-07 20:00:16 -0300

    add example test

---
+++ src/index.spec.ts
@@ -0,0 +1,11 @@
+import 'should';
+
+describe('Testing test', () => {
+    it('should true the true', () => {
+        true.should.true();
+    });
+
+    it('should false the false', () => {
+        true.should.false();
+    });
+});
