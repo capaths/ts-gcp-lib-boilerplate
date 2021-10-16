6dc68b19071fbda18f9fd47c2d238954e6c9d8ef
Paths <pcampaa@gmail.com>
2021-10-15 21:25:55 -0300

    add missing set-up variables

--- scripts/setup.js
+++ scripts/setup.js
@@ -5,32 +5,45 @@ const path = require('path');
 const PACKAGE_JSON = path.join(path.dirname(__filename), '../package.json');
 
 function ask(query, defaultValue) {
-    const answer = readline.question(`${query} (${defaultValue}): `);
+    const answer = readline.question(`${query} ("${defaultValue}"): `);
     return answer.length === 0 ? defaultValue : answer;
 }
 
-const scope = ask("Scope", "my-scope");
-const packageName = ask("Package name", "my-package");
-const author = ask("Author", "me");
-const location = ask("GCP location", "us-central1");
-const repository = ask("GCP AR repository", "my-repository");
-
 const packageJson = {...require('../package.json')};
+
+const scope = ask("Scope", packageJson.config.scope);
+const packageName = ask("Package name", packageJson.config.name);
+const author = ask("Author", packageJson.author);
+const project = ask("GCP Project ID", packageJson.config.project);
+const location = ask("GCP location", packageJson.config.location);
+const repository = ask("GCP AR repository", scope);
+const description = ask("Package description", packageJson.description);
+
 packageJson.name = `@${scope}/${packageName}`;
 packageJson.author = author;
+packageJson.description = description;
 packageJson.config = {
     ...packageJson.config,
     "scope": scope,
     "name": packageName,
     "location": location,
-    "repository": repository
+    "repository": repository,
+    "project": project
 };
 
-const npmrcBuild = "gcloud artifacts print-settings npm --scope=@$npm_package_config_scope --location=$npm_package_location --repository=$npm_package_config_repository --project nirby-prod > .npmrc"
-    .replaceAll('$npm_package_config_scope', scope)
-    .replaceAll('$npm_package_location', scope)
-    .replaceAll('$npm_package_config_repository', scope);
+const npmRcBuild = "gcloud artifacts print-settings npm --scope=@$npm_package_config_scope --location=$npm_package_location --repository=$npm_package_config_repository --project $npm_package_config_project > .npmrc"
+    .replace(/\$npm_package_config_scope/g, scope)
+    .replace(/\$npm_package_location/g, location)
+    .replace(/\$npm_package_config_repository/g, repository)
+    .replace(/\$npm_package_config_project/g, project);
 
-packageJson.scripts["npmrc:build"] = npmrcBuild;
+packageJson.scripts["npmrc:build"] = npmRcBuild;
 
-fs.writeFileSync(PACKAGE_JSON, JSON.stringify(packageJson, undefined, 4), {encoding: 'utf8',flag:'w'});
+fs.writeFileSync(
+    PACKAGE_JSON,
+    JSON.stringify(packageJson, undefined, 4),
+    {
+        encoding: 'utf8',
+        flag: 'w'
+    }
+);
